cd735ffc42f014b57eafab7f315b1be4808d6da9
Craig Carlyle <craig.carlyle@clio.com>
2020-04-10 14:55:42 -0700

    Adds convertUnixTimeToString helper

--- src/Helpers.js
+++ src/Helpers.js
@@ -6,3 +6,8 @@ export const debounce = (func, wait = 0) => {
     timer = setTimeout(func, wait, ...args);
   };
 };
+
+export const convertUnixTimeToString = (unixTime) => {
+  const jsDate = new Date(unixTime * 1000);
+  return jsDate.toLocaleString();
+};
