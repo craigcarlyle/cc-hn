790feec0b8d7f35d94745d637c6848c9216a8e0a
Craig Carlyle <craig.carlyle@clio.com>
2020-04-11 11:27:33 -0700

    Adds specs for App, HackerNewsList

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import { mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import { HackerNewsList } from "./HackerNewsList";
+import { useAjax } from "./customHooks";
+
+jest.mock("./customHooks");
+
+configure({ adapter: new Adapter() });
+
+describe("while loading", () => {
+  it("renders the loader element", () => {
+    useAjax.mockReturnValue([null, true, null]);
+
+    const { getByTestId } = render(<App />);
+    const loaderElement = getByTestId("main-loader");
+
+    expect(loaderElement).toBeInTheDocument();
+    expect(loaderElement).toHaveTextContent("Fetching stories...");
+  });
 });
+
+describe("when an error is returned", () => {
+  it("renders the error element", () => {
+    useAjax.mockReturnValue([null, null, true]);
+
+    const { getByTestId } = render(<App />);
+    const errorElement = getByTestId("main-error");
+
+    expect(errorElement).toBeInTheDocument();
+    expect(errorElement).toHaveTextContent("Error fetching stories.");
+  });
+});
+
+describe("when the fetch is successful", () => {
+  it("passes the array of IDs to the HackerNewsList component", () => {
+    const storyIDs = [1, 2, 3];
+    const hackerNewsListComponent = mount(
+      <HackerNewsList storyIDs={storyIDs} />
+    );
+
+    useAjax.mockReturnValue([storyIDs, false, false]);
+    expect(hackerNewsListComponent.props().storyIDs).toBe(storyIDs);
+  });
+});
+
+afterEach(cleanup);
---
+++ src/HackerNewsList.test.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { render, cleanup, act } from "@testing-library/react";
+import { mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import { HackerNewsList } from "./HackerNewsList";
+import { range } from "./helpers";
+
+configure({ adapter: new Adapter() });
+
+describe("when the storyIDs are passed to the component", () => {
+  it("populates the list with the first 10 IDs", () => {
+    const initialIDs = range(0, 499);
+    const { getAllByTestId } = render(<HackerNewsList storyIDs={initialIDs} />);
+    const storyListItemElements = getAllByTestId("list-item-loader");
+
+    expect(storyListItemElements.length).toBe(10);
+  });
+});
+
+describe("when the storyIDs are NOT passed to the component", () => {
+  it("fetches the entire list from local storage", () => {
+    Storage.prototype.getItem = jest.fn(() => JSON.stringify(range(0, 499)));
+
+    const { getAllByTestId } = render(<HackerNewsList />);
+    const storyListItemElements = getAllByTestId("list-item-loader");
+
+    expect(storyListItemElements.length).toBe(500);
+  });
+});
+
+describe("when a scroll event is triggered", () => {
+  describe("at the end of the content", () => {
+    let hackerNewsListComponent;
+    let localStorageSpy;
+
+    beforeAll(() => {
+      // Use fake timers for debounced function
+      jest.useFakeTimers();
+
+      const initialIDs = range(0, 499);
+
+      global.innerHeight = 1000;
+      jest.spyOn(global.document, "documentElement", "get").mockReturnValue({
+        scrollTop: 1000,
+        offsetHeight: 2000,
+      });
+
+      hackerNewsListComponent = mount(<HackerNewsList storyIDs={initialIDs} />);
+      localStorageSpy = jest.spyOn(Storage.prototype, "setItem");
+
+      act(() => {
+        global.dispatchEvent(new Event("scroll"));
+        // Run timers to account for debounced function
+        jest.runAllTimers();
+      });
+    });
+
+    it("should load 10 more stories", () => {
+      expect(hackerNewsListComponent.getDOMNode().childElementCount).toBe(20);
+    });
+
+    it("should update localStorage with the newly added IDs", () => {
+      expect(localStorageSpy).toHaveBeenCalledWith(
+        "currentStoryIDs",
+        JSON.stringify(range(0, 19))
+      );
+    });
+  });
+
+  describe("NOT at the end of the content", () => {
+    it("should NOT load more stories", () => {
+      global.innerHeight = 1000;
+      jest.spyOn(global.document, "documentElement", "get").mockReturnValue({
+        scrollTop: 0,
+        offsetHeight: 2000,
+      });
+
+      const initialIDs = range(0, 499);
+      const hackerNewsListComponent = mount(
+        <HackerNewsList storyIDs={initialIDs} />
+      );
+
+      act(() => {
+        global.dispatchEvent(new Event("scroll"));
+      });
+
+      expect(hackerNewsListComponent.getDOMNode().childElementCount).toBe(10);
+    });
+  });
+});
+
+afterEach(cleanup);
